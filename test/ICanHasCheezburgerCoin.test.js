a90ea3509e47325f341f1402a8ccc75cce070d08
Mitscoin-MTC <114988738+Johansmith007@users.noreply.github.com>
2022-10-10 03:27:58 -0500

    Update ICanHasCheezburgerCoin.test.js

--- test/ICanHasCheezburgerCoin.test.js
+++ test/ICanHasCheezburgerCoin.test.js
@@ -1,42 +1,92 @@
-const ICanHasCheezburgerToken = artifacts.require("ICanHasCheezburgerToken")
+const { expect } = require("chai");
+const hre = require("hardhat");
 
-contract("ICanHasCheezburgerToken", (accounts) => {
-  before(async () => {
-      iCHCToken = await ICanHasCheezburgerToken.deployed()
-      console.log(`Address: ${iCHCToken.address}`)
-  })
+describe("mitscoin contract", function() {
+  // global vars
+  let Token;
+  let mitscoin;
+  let owner;
+  let addr1;
+  let addr2;
+  let tokenCap = 100000000;
+  let tokenBlockReward = 50;
 
-  it('the token cap should be 100 million tokens', async () => {
-    let cap = await iCHCToken.cap()
-    cap = web3.utils.fromWei(cap,'ether')
-    assert.equal(cap, 100000000, 'The token cap should be 100 million')
-  })
+  beforeEach(async function () {
+    // Get the ContractFactory and Signers here.
+    Token = await ethers.getContractFactory("mitscoin");
+    [owner, addr1, addr2] = await hre.ethers.getSigners();
 
-  it('gives the owner of the contract 69 million tokens', async () => {
-    let balance = await iCHCToken.balanceOf(accounts[0])
-    balance = web3.utils.fromWei(balance,'ether')
-    assert.equal(balance, 69000000, 'Balance of contract creator account should be sixty nine million')
-  })
-
-  it('total supply updated after contract creation & transfer to owner', async () => {
-    let uncirculatedSupply = await iCHCToken.totalSupply()
-    uncirculatedSupply = web3.utils.fromWei(uncirculatedSupply,'ether')
-    assert.equal(uncirculatedSupply, 69000000, 'Total supply should now be sixty nine million')
-  })
-
-  it('can transfer tokens between accounts', async () => {
-    let amount = web3.utils.toWei('1000','ether')
-    await iCHCToken.transfer(accounts[1], amount, {from: accounts[0]})
-
-    let balance = await iCHCToken.balanceOf(accounts[1])
-    balance = web3.utils.fromWei(balance,'ether')
-    assert.equal(balance, 1000, 'Balance of recipient account should be one thousand')
-  })
-
-  it('owner can set block reward', async () => {
-    await iCHCToken.setBlockReward(50)
-    let blockReward = await iCHCToken.getBlockReward()
-    blockReward = web3.utils.fromWei(blockReward,'ether')
-    assert.equal(blockReward, 50, 'Block reward should be fifty')
+    mitscoin = await Token.deploy(tokenCap, tokenBlockReward);
   });
-})
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      expect(await mitscoin.owner()).to.equal(owner.address);
+    });
+
+    it("Should assign the total supply of tokens to the owner", async function () {
+      const ownerBalance = await mitscoin.balanceOf(owner.address);
+      expect(await mitscoin.totalSupply()).to.equal(ownerBalance);
+    });
+
+    it("Should set the max capped supply to the argument provided during deployment", async function () {
+      const cap = await mitscoin.cap();
+      expect(Number(hre.ethers.utils.formatEther(cap))).to.equal(tokenCap);
+    });
+
+    it("Should set the blockReward to the argument provided during deployment", async function () {
+      const blockReward = await mitscoin.blockReward();
+      expect(Number(hre.ethers.utils.formatEther(blockReward))).to.equal(tokenBlockReward);
+    });
+  });
+
+  describe("Transactions", function () {
+    it("Should transfer tokens between accounts", async function () {
+      // Transfer 50 tokens from owner to addr1
+      await mitscoin.transfer(addr1.address, 50);
+      const addr1Balance = await mitscoin.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(50);
+
+      // Transfer 50 tokens from addr1 to addr2
+      // We use .connect(signer) to send a transaction from another account
+      await mitscoin.connect(addr1).transfer(addr2.address, 50);
+      const addr2Balance = await mitscoin.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(50);
+    });
+
+    it("Should fail if sender doesn't have enough tokens", async function () {
+      const initialOwnerBalance = await mitscoin.balanceOf(owner.address);
+      // Try to send 1 token from addr1 (0 tokens) to owner (1000000 tokens).
+      // `require` will evaluate false and revert the transaction.
+      await expect(
+        mitscoin.connect(addr1).transfer(owner.address, 1)
+      ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+
+      // Owner balance shouldn't have changed.
+      expect(await mitscoin.balanceOf(owner.address)).to.equal(
+        initialOwnerBalance
+      );
+    });
+
+    it("Should update balances after transfers", async function () {
+      const initialOwnerBalance = await mitscoin.balanceOf(owner.address);
+
+      // Transfer 100 tokens from owner to addr1.
+      await mitscoin.transfer(addr1.address, 100);
+
+      // Transfer another 50 tokens from owner to addr2.
+      await mitscoin.transfer(addr2.address, 50);
+
+      // Check balances.
+      const finalOwnerBalance = await mitscoin.balanceOf(owner.address);
+      expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(150));
+
+      const addr1Balance = await mitscoin.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(100);
+
+      const addr2Balance = await mitscoin.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(50);
+    });
+  });
+  
+});
